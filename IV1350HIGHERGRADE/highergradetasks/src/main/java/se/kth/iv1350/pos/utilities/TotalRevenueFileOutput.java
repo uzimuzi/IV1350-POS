bc4d8a6695c783108d29b260f1109b58ff746754
uzimuzi <usseman@gmail.com>
2025-06-15 01:07:08 +0200

    Higher grade tasks fixed after feedback.

--- IV1350HIGHERGRADE/highergradetasks/src/main/java/se/kth/iv1350/pos/utilities/TotalRevenueFileOutput.java
+++ IV1350HIGHERGRADE/highergradetasks/src/main/java/se/kth/iv1350/pos/utilities/TotalRevenueFileOutput.java
@@ -4,13 +4,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import se.kth.iv1350.pos.observer.RevenueObserver;
+import se.kth.iv1350.pos.view.RevenueDisplayTemplate;
 /**
  * The file that writes the revenue to a file.
  */
-public class TotalRevenueFileOutput implements RevenueObserver{
+public class TotalRevenueFileOutput extends RevenueDisplayTemplate{
     
-    private double totalRevenue;
     private PrintWriter revenueWriter;
 
     /**
@@ -21,14 +20,25 @@ public class TotalRevenueFileOutput implements RevenueObserver{
     public TotalRevenueFileOutput(String file) throws IOException{
         revenueWriter = new PrintWriter(new FileWriter(file, true));
     }
+
     /**
-     * Updates the total revenue and writes to the file.
+     * Writes total to the file.
      */
-    public void newRevenue(double revenue){
-        totalRevenue += revenue;
-        revenueWriter.println("Total revenue: " + totalRevenue + " SEK");
+    @Override
+    protected void showRevenue() throws IOException{
+        revenueWriter.println("Total revenue: " + getTotalRevenue() + " SEK");
         revenueWriter.flush();
     }
+
+    /**
+     * Handles exceptions that when writing to the file.
+     */
+    @Override
+    protected void handleException(Exception error){
+        System.out.println("Error writing revenue to file: " + error.getMessage());
+    }
+    
+
     /**
      * Closes the writer.
      */
